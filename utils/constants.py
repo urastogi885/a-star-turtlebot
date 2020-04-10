0bbe6994aa7e5d9fa1b8d9f19485694285159d0f
umang <umang.rastogi96@gmail.com>
2020-04-10 12:47:30 -0400

    Add constant for center position of the map

--- utils/constants.py
+++ utils/constants.py
@@ -6,13 +6,13 @@ goal_thresh = int(scaling_factor * 0.1)
 # Define map size
 width, height = 10, 10
 map_size = (scaling_factor * height), (scaling_factor * width)
+map_center = (map_size[0] // 2), (map_size[1] // 2)
 # Define robot parameters in cm
 robot_diameter = 21
 robot_radius = robot_diameter / 2
 wheel_distance = 16
-# Define all the possible no. of actions
+# Define the maximum no. of possible actions
 max_actions = 8
-half_actions = max_actions // 2
 # Robot moves by 1 unit (cm): translation-step
 # Orientation of the robot is a multiple of angular-step
 angular_step = 30
