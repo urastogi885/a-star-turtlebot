04894aa390087af189e042adb5428522685643d2
umang <umang.rastogi96@gmail.com>
2020-04-11 12:50:50 -0400

    Add comments for better code documentation

--- utils/explorer.py
+++ utils/explorer.py
@@ -27,6 +27,12 @@ def check_node_validity(check_img, x, y):
 
 
 def get_euclidean_distance(first_node, second_node):
+    """
+    Get euclidean distance between two points
+    :param first_node: a tuple containing coordinates of first point
+    :param second_node: a tuple containing coordinates of second point
+    :return: euclidean distance between two points
+    """
     return sqrt((second_node[0] - first_node[0]) ** 2 + (second_node[1] - first_node[1]) ** 2)
 
 
@@ -55,7 +61,9 @@ class Explorer:
         self.goal_node = goal_node
         # Store RPMs of robot
         self.robot_rpm = robot_rpm
+        # Original map
         self.map_img = map_img[0]
+        # Extended map due to robot radius and clearance
         self.check_img = map_img[1]
         # Store angular step size and translation step size
         self.step_theta = constants.angular_step
@@ -66,8 +74,6 @@ class Explorer:
         self.path_nodes = []
         # Define 3-D arrays to store information about generated nodes and parent nodes
         self.parent = np.full(fill_value=constants.no_parent, shape=self.map_size)
-        # Define grey
-        self.grey = [128, 128, 128]
         # Define video-writer of open-cv to record the exploration and final path
         video_format = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
         self.video_output = cv2.VideoWriter('video_output.mp4', video_format, 600.0,
@@ -94,25 +100,34 @@ class Explorer:
         return self.get_heuristic_score(node) + base_cost
 
     def get_orientation_index(self, theta):
-        # Get orientation of child node in degrees
+        """
+        Convert theta from radians to a grid world index based on pre-defined angular step
+        :param theta: orientation of the robot in radians
+        :return: index of orientation based on angular step
+        """
+        # Limit orientation between 0 to 360
         if theta >= 2 * np.pi:
             n = int(theta / (2 * np.pi))
             theta = theta - n * 2 * np.pi
         elif theta < 0:
+            # Convert negative angle into positive and maintain the orientation between 0 to 360
             theta = abs(theta)
             if theta > 2 * np.pi:
                 n = int(theta / (2 * np.pi))
                 theta = theta - n * 2 * np.pi
+        # Get orientation in degrees
         theta = theta + (180 * theta / np.pi)
+        # Limit orientation between 0 to 360
         if theta > constants.total_angle:
             theta = theta - constants.total_angle
+        # Return index by dividing orientation in degrees by angular step
         return int(theta / self.step_theta)
 
     def action_space(self, action):
         """
         Define action space
         :param action: Varies from 0-7 to call one of the 8 defined actions
-        :return: a tuple of left and right wheel RPM
+        :return: a tuple of left and right wheel RPM respectively
         """
         if action == 0:
             return 0, self.robot_rpm[0]
@@ -137,18 +152,14 @@ class Explorer:
         Applying differential drive formulae to get coordinates and orientation
         :param parent_node: a tuple of parent node coordinates and orientation
         :param action: Varies from 0-n to call one of the 8 defined actions
-        :return: new coordinates and orientation of the node after the desired action
+        :return: child node
         """
-        # Get parent coordinates and orientation in radians
-        parent_node_data = parent_node.get_data()
-        y, x = parent_node_data[0], parent_node_data[1]
-        theta = np.pi * parent_node_data[2] * self.step_theta / 180
         # Get action to be performed on parent to generate child
         rpm = self.action_space(action)
         # Convert rpm into left and right wheel velocities respectively
         lw_velocity = rpm[0] * (constants.wheel_radius + (0.5 * constants.wheel_distance)) * (2 * np.pi / 60)
         rw_velocity = rpm[1] * (constants.wheel_radius - (0.5 * constants.wheel_distance)) * (2 * np.pi / 60)
-        return self.show_exploration(parent_node, x, y, theta, lw_velocity, rw_velocity)
+        return self.get_child_node(parent_node, lw_velocity, rw_velocity)
 
     def explore(self):
         """
@@ -179,16 +190,25 @@ class Explorer:
                     # Add child node to priority queue
                     node_queue.put(child_node)
 
-    def show_exploration(self, parent_node, x, y, theta, l_vel, r_vel):
+    def get_child_node(self, parent_node, l_vel, r_vel):
         """
-        Show animation of map exploration and path from start to goal
-        :return: nothing
+        Get child node based on wheel velocities
+        Show exploration by generating intermediate nodes
+        :param parent_node: node class object of parent
+        :param l_vel: left-wheel velocity of robot
+        :param r_vel: right-wheel velocity of robot
+        :return: node class object of child
         """
         # An empty list to store intermediate nodes
         inter_nodes = []
         valid_path = True
+        # Define grey
+        grey = [128, 128, 128]
+        # Get coordinates  and orientation of parent node
         parent_node_data = parent_node.get_data()
-        prev_x, prev_y, prev_theta = parent_node_data[1], parent_node_data[0], parent_node_data[2]
+        y, x = parent_node_data[0], parent_node_data[1]
+        theta = np.pi * parent_node_data[2] * self.step_theta / 180
+        prev_y, prev_x, prev_theta = y, x, parent_node_data[2]
         t = 0
         while t < constants.total_time:
             # Increment time
@@ -200,29 +220,36 @@ class Explorer:
                   constants.time_scaling)
             theta += ((constants.robot_radius / constants.wheel_distance) * (r_vel - l_vel) *
                       constants.time_step * constants.time_scaling)
+            # Get index of current orientation in grid world to check for parent
             theta_index = self.get_orientation_index(theta)
+            # Check for validity of intermediate node
             if (check_node_validity(self.check_img, int(x), self.map_size[0] - int(y)) and
                     self.parent[int(y)][int(x)][theta_index] == constants.no_parent):
-                # Plot curve on map
+                # Add intermediate to its list
                 inter_nodes.append(([prev_y, prev_x, prev_theta], [y, x, theta]))
+                # Update previous coordinates and orientation
                 prev_x, prev_y, prev_theta = x, y, theta
             else:
                 valid_path = False
                 break
+        # Discard entire path if any intermediate point lies within obstacle space
         if valid_path:
             last_node = None
             len_inter_nodes = len(inter_nodes)
+            # Add exploration to video
             for i in range(len_inter_nodes):
                 prev_node, current_node = inter_nodes[i]
-                # Get orientation of child node in degrees
+                # Get index of orientation of intermediate node
                 prev_node[2] = self.get_orientation_index(prev_node[2])
                 current_node[2] = self.get_orientation_index(current_node[2])
-                # Update parent of the child node
+                # Update parent of the intermediate node
                 self.parent[int(current_node[0])][int(current_node[1])][int(current_node[2])] = \
                     np.ravel_multi_index([int(prev_node[0]), int(prev_node[1]), int(prev_node[2])], dims=self.map_size)
+                # Add nodes to exploration video
                 cv2.arrowedLine(self.map_img, (int(prev_node[1]), self.map_size[0] - int(prev_node[0])),
-                                (int(current_node[1]), self.map_size[0] - int(current_node[0])), self.grey)
+                                (int(current_node[1]), self.map_size[0] - int(current_node[0])), grey)
                 self.video_output.write(self.map_img)
+                # Make last node in the list as the child node and create is node class object
                 if i == len_inter_nodes - 1:
                     last_node = Node(current_node, parent_node_data, float('inf'), float('inf'), inter_nodes)
                     last_node.set_base_weight(get_base_cost(parent_node, current_node))
@@ -235,14 +262,16 @@ class Explorer:
         Generate path using back-tracking
         :return: a list containing path nodes
         """
+        # Exit if exploration failed
         if not len(self.path_nodes):
             print('No path found')
             return False
+        # Define colors
         red = [0, 0, 255]
         blue = [255, 0, 0]
         green = [0, 255, 0]
+        # Get first node nearest to the goal node to start backtracking
         last_node = self.path_nodes[0]
-        # print(np.where(self.parent == constants.start_parent))
         print('Finding path...')
         # Iterate until we reach the initial node
         while last_node.get_data() != self.start_node.get_data():
@@ -252,9 +281,11 @@ class Explorer:
                     last_node = node
                     break
         print('Path found')
-        # Show path
+        # Iterate through path nodes
         for i in range(len(self.path_nodes) - 2, 0, -1):
+            # Get intermediate nodes of each node in path-nodes' list
             current_sub_nodes = self.path_nodes[i].get_sub_nodes()
+            # Iterate through intermediate nodes' list to display path to be taken by the robot
             for j in range(0, len(current_sub_nodes)):
                 current_node_data = current_sub_nodes[j]
                 cv2.line(self.map_img, (int(current_node_data[0][1]), self.map_size[0] - int(current_node_data[0][0])),
@@ -265,6 +296,7 @@ class Explorer:
                    int(constants.robot_radius), red, -1)
         cv2.circle(self.map_img, (int(self.path_nodes[0].data[1]), self.map_size[0] - int(self.path_nodes[0].data[0])),
                    int(constants.robot_radius), green, -1)
+        # Show path for longer time
         for _ in range(1000):
             self.video_output.write(self.map_img)
         return True
